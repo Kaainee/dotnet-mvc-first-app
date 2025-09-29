f6e7976589f92b73013294daea8e93f38f3c967b
naufalaf4@gmail.com <naufalaf4@gmail.com>
2025-09-30 02:51:28 +0700

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mvc_first_app.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
